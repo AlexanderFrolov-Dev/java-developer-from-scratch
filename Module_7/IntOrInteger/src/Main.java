749aca25d31501599ad897b0d840d47034a54623
AlexanderFrolov-spec <84732086+AlexanderFrolov-spec@users.noreply.github.com>
2022-10-17 21:04:19 +0300

    Написал код для вывода всех букв русского алфавита с их номерами в юникоде.

--- Module_7/IntOrInteger/src/Main.java
+++ Module_7/IntOrInteger/src/Main.java
@@ -8,5 +8,15 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        char uppercaseA = 'А';
+        char ya = 'я';
+        char yo = 'ё';
+        char uppercaseYo = 'Ё';
+
+        for (int i = 0; i <= yo; i++) {
+            if ((i >= uppercaseA && i <= ya) || i == yo || i == uppercaseYo) {
+                System.out.println(i + " - " + (char) i);
+            }
+        }
     }
 }
