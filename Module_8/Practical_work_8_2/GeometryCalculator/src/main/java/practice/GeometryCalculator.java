98391f4d11304983ee61e1c6d5c31d5bc2a7baf9
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-18 12:27:06 +0300

    Написал все методы в классе GeometryCalculator. Все тесты проходят.

--- Module_8/Practical_work_8_2/GeometryCalculator/src/main/java/practice/GeometryCalculator.java
+++ Module_8/Practical_work_8_2/GeometryCalculator/src/main/java/practice/GeometryCalculator.java
@@ -4,21 +4,25 @@ public class GeometryCalculator {
 
     // если значение radius меньше 0, метод должен вернуть -1
     public static double getCircleSquare(double radius) {
-        return 0.0;
+        double square = Math.PI * Math.pow(radius, 2);
+        return radius >= 0 ? square : -1;
     }
 
     // если значение radius меньше 0, метод должен вернуть -1
     public static double getSphereVolume(double radius) {
-        return 0.0;
+        double volume = ((double) 4 / 3) * Math.PI * Math.pow(radius, 3);
+        return radius >= 0 ? volume : -1;
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        return (a + b) > c && (a + c) > b && (b + c) > a;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0.0;
+        double p = (a + b + c) / 2;
+        double square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return isTrianglePossible(a, b, c) ? square : -1;
     }
 }
