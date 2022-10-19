f49edf42019757040b86b854cf7e49dbba6e51bd
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-19 17:53:32 +0300

    8.13 выполнил задания в классе Birthdays и Periods. Все тесты проходят успешно.

--- Module_8/Control_practical_work_8_13/NumbersAndDates_2/src/main/java/practice/Birthdays.java
+++ Module_8/Control_practical_work_8_13/NumbersAndDates_2/src/main/java/practice/Birthdays.java
@@ -1,23 +1,37 @@
 package practice;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+
 public class Birthdays {
 
     public static void main(String[] args) {
-
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 15;
+        int month = 7;
+        int year = 1984;
 
         System.out.println(collectBirthdays(year, month, day));
-
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate currentDate = LocalDate.now();
+        int age = (int) birthday.until(currentDate, ChronoUnit.YEARS);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E").localizedBy(new Locale("us"));
+        String result = "";
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+
+        if (currentDate.isAfter(birthday) || currentDate.isEqual(birthday)) {
+            for (int i = 0; i <= age; i++) {
+                LocalDate nextBirthday = birthday.plusYears(i);
+                result += i + " - " + formatter.format(nextBirthday) + System.lineSeparator();
+            }
+        }
+
+        return result;
     }
 }
--- Module_8/Control_practical_work_8_13/NumbersAndDates_2/src/main/java/practice/Periods.java
+++ Module_8/Control_practical_work_8_13/NumbersAndDates_2/src/main/java/practice/Periods.java
@@ -1,12 +1,14 @@
 package practice;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Periods {
 
     // реализуйте вывод разницы между датами, используя класс Period
     public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
-        return "";
-    }
+        Period period = firstDate.until(secondDate);
 
+        return "years:" + period.getYears() + ", months:" + period.getMonths() + ", days:" + period.getDays();
+    }
 }
