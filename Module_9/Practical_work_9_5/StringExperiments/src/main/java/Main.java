0be6f6df5c7f25b048894afafc5f751b2f6bf0d7
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-24 13:42:56 +0300

    Выполнил второе задание к уроку 9.5.

--- Module_9/Practical_work_9_5/StringExperiments/src/main/java/Main.java
+++ Module_9/Practical_work_9_5/StringExperiments/src/main/java/Main.java
@@ -1,17 +1,35 @@
 public class Main {
     public static void main(String[] args) {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int firstDigit;
-        int lastDigit;
 
-        // TODO: 21.10.2022 Поделить пробелами строку на блоки. Потом проверять состоит ли подстрока только из цифр.
+        System.out.println("Общая сумма заработанных денег: " + getSubstrings(text));
+    }
+
+    public static int getSubstrings(String text) {
+        int sum = 0;
 
         for (int i = 0; i < text.length(); i++) {
-            char symbol = (char) i;
-
-            if (Character.isDigit(symbol)) {
-                firstDigit = text.charAt(i);
+            if (text.indexOf(' ') > 0) {
+                String currentSubstring = text.substring(0, text.indexOf(' '));
+                text = text.substring(currentSubstring.length() + 1);
+                if (isNumber(currentSubstring)) {
+                    sum += Integer.parseInt(currentSubstring);
+                }
             }
         }
+
+        return sum;
+    }
+
+    public static boolean isNumber(String substring) {
+        boolean isNumber = true;
+
+        for (int i = 0; i < substring.length(); i++) {
+            if (!Character.isDigit(substring.charAt(i))) {
+                isNumber = false;
+            }
+        }
+
+        return isNumber;
     }
 }
