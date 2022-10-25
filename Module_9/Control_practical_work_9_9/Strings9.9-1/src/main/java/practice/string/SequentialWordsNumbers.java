e0068c94d2731a34dc06697613a36a41e61df132
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-25 11:05:24 +0300

    Выполнил задание 1 в классе SequentialWordsNumbers к контрольной практической работе 9.9. Все тесты проходят.

--- Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/string/SequentialWordsNumbers.java
+++ Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/string/SequentialWordsNumbers.java
@@ -2,7 +2,23 @@ package practice.string;
 
 public class SequentialWordsNumbers {
 
-    public static String sequentialWordsNumbers(String text){
-        return "";
+    public static String sequentialWordsNumbers(String text) {
+        StringBuilder sb = new StringBuilder();
+        String substr;
+        int count = 1;
+
+        while (text.indexOf(' ') > 0) {
+            substr = text.substring(0, text.indexOf(' '));
+            sb.append("(").append(count).append(") ").append(substr).append(" ");
+            text = text.substring(substr.length() + 1);
+            count++;
+        }
+
+        if (!text.isBlank()) {
+            substr = text;
+            sb.append("(").append(count).append(") ").append(substr).append(" ");
+        }
+
+        return sb.toString().trim();
     }
 }
