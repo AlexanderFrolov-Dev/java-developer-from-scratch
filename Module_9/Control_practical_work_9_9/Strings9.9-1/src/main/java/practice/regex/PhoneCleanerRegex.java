619a36cd12a2a888054e4eefbd4458a51e6e4bfe
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-25 16:56:57 +0300

    Выполнил задание 3 в классе PhoneCleanerRegex к контрольной практической работе 9.9 с помощью группировки в регулярном выражении. Все тесты проходят.

--- Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/regex/PhoneCleanerRegex.java
+++ Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/regex/PhoneCleanerRegex.java
@@ -6,6 +6,21 @@ import java.util.regex.Pattern;
 
 public class PhoneCleanerRegex {
     public static void main(String[] args) {
+        /*
+        Группы в регулярном выражении:
+            $1 - (\+?)
+            $2 - (\D*)
+            $3 - ([0-9]?)
+            $4 - (\D*)
+            $5 - (9([0-9]{2}))
+            $6 - ([0-9]{2}) - группа 5 является вложенной в группе 6
+            $7 - (\D*)
+            $8 - ([0-9]{3})
+            $9 - (\D*)
+            $10 - ([0-9]{2})
+            $11 - (\D*)
+            $12 - ([0-9]{2})
+         */
         Scanner scanner = new Scanner(System.in);
         String correctPhoneNumber;
         Pattern pattern = Pattern.compile("(\\+?)(\\D*)([0-9]?)(\\D*)(9([0-9]{2}))(\\D*)([0-9]{3})(\\D*)([0-9]{2})(\\D*)([0-9]{2})");
@@ -19,10 +34,7 @@ public class PhoneCleanerRegex {
                 break;
             }
             if (matcher.matches()) {
-                correctPhoneNumber = matcher.group().replaceAll("[\\D+]", "");
-                correctPhoneNumber = correctPhoneNumber.length() == 10
-                        ? "7" + correctPhoneNumber
-                        : correctPhoneNumber.replaceFirst("\\d", "7");
+                correctPhoneNumber = matcher.replaceFirst("7$5$8$10$12");
             } else {
                 correctPhoneNumber = "Неверный формат номера";
             }
