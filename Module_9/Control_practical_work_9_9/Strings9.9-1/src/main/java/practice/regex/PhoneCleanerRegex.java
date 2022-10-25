b45e917980a96933ec450a05f0a3863351968ce6
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-25 16:24:38 +0300

    Выполнил задание 3 в классе PhoneCleanerRegex к контрольной практической работе 9.9. Все тесты проходят.

--- Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/regex/PhoneCleanerRegex.java
+++ Module_9/Control_practical_work_9_9/Strings9.9-1/src/main/java/practice/regex/PhoneCleanerRegex.java
@@ -1,21 +1,33 @@
 package practice.regex;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneCleanerRegex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String correctPhoneNumber;
+        Pattern pattern = Pattern.compile("(\\+?)(\\D*)([0-9]?)(\\D*)(9([0-9]{2}))(\\D*)([0-9]{3})(\\D*)([0-9]{2})(\\D*)([0-9]{2})?");
 
-  public static void main(String[] args) {
+        while (true) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        scanner.close();
-        break;
-      }
+            if (input.equals("0")) {
+                scanner.close();
+                break;
+            }
+            if (matcher.matches()) {
+                correctPhoneNumber = matcher.group().replaceAll("[\\D+]", "");
+                correctPhoneNumber = correctPhoneNumber.length() == 10
+                        ? "7" + correctPhoneNumber
+                        : correctPhoneNumber.replaceFirst("\\d", "7");
+            } else {
+                correctPhoneNumber = "Неверный формат номера";
+            }
 
-      // TODO:напишите ваш код тут, результат вывести в консоль.
+            System.out.println(correctPhoneNumber);
+        }
     }
-  }
-
 }
