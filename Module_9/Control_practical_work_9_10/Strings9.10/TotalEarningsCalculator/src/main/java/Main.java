41bdecf2c877615c207e27b92cf05e2e312c3956
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-26 09:53:53 +0300

    Выполнил задание 1 (проект TotalEarningsCalculator) к контрольной практической работе 9.10. Все тесты проходят.

--- Module_9/Control_practical_work_9_10/Strings9.10/TotalEarningsCalculator/src/main/java/Main.java
+++ Module_9/Control_practical_work_9_10/Strings9.10/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,36 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        //Код взят из практической работы 9.5.
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        System.out.println(getSubstrings(text));
+    }
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+    public static int getSubstrings(String text) {
+        int sum = 0;
 
+        for (int i = 0; i < text.length(); i++) {
+            if (text.indexOf(' ') > 0) {
+                String currentSubstring = text.substring(0, text.indexOf(' '));
+                text = text.substring(currentSubstring.length() + 1);
+                if (isNumber(currentSubstring)) {
+                    sum += Integer.parseInt(currentSubstring);
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    public static boolean isNumber(String substring) {
+        boolean isNumber = true;
+
+        for (int i = 0; i < substring.length(); i++) {
+            if (!Character.isDigit(substring.charAt(i))) {
+                isNumber = false;
+            }
+        }
+
+        return isNumber;
+    }
 }
