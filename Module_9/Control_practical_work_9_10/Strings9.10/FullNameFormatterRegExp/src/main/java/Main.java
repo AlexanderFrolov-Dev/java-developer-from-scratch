c4586ebedd7b299852a9673603f0225f2a7e0330
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-27 14:41:02 +0300

    Выполнил задание 5 (проект FullNameFormatterRegExp) в контрольной практической работе 9.10. Все тесты проходят.

--- Module_9/Control_practical_work_9_10/Strings9.10/FullNameFormatterRegExp/src/main/java/Main.java
+++ Module_9/Control_practical_work_9_10/Strings9.10/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String[] words = input.split("\\s+");
+            if (input.equals("0")) {
+                break;
+            }
+
+            if (words.length == 3 && isValidWords(words)) {
+                System.out.println("Фамилия: ".concat(words[0]).concat(System.lineSeparator())
+                        .concat("Имя: ").concat(words[1]).concat(System.lineSeparator())
+                        .concat("Отчество: ").concat(words[2]));
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+            }
+        }
     }
-  }
+
+    private static boolean isValidWords(String[] words) {
+        Pattern pattern = Pattern.compile("[А-ЯЁа-яё+]-?[А-ЯЁа-яё+]?");
+        boolean isValid = true;
+
+        for (String word : words) {
+            Matcher matcher = pattern.matcher(word);
+            if (!matcher.find()) {
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    }
 
 }
