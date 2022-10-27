6afadbf47a79758e3a7f60a3f9d1197945b1827f
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-27 17:50:40 +0300

    Переименовал папку Practical_work_10_5 в Control_practical_work_10_5 и выполнил первое задание.

---
+++ Module_10/Control_practical_work_10_5/CollectionsArray/src/main/java/practice/reverseArray/ReverseArray.java
@@ -0,0 +1,16 @@
+package practice.reverseArray;
+
+public class ReverseArray {
+
+    public static String[] reverse(String[] strings) {
+
+        for (int i = 0; i < strings.length / 2; i++) {
+            String element;
+            element = strings[i];
+            strings[i] = strings[strings.length - i - 1];
+            strings[strings.length - i - 1] = element;
+        }
+        return strings;
+    }
+
+}
