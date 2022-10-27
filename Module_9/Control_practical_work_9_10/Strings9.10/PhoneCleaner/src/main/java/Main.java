4dd435471116dd32e61f407d19f34fcbd17603b4
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-27 15:08:15 +0300

    Выполнил задание 6 (проект PhoneCleaner) в контрольной практической работе 9.10. Все тесты проходят.

--- Module_9/Control_practical_work_9_10/Strings9.10/PhoneCleaner/src/main/java/Main.java
+++ Module_9/Control_practical_work_9_10/Strings9.10/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        String correctPhoneNumber;
+        Pattern pattern = Pattern.compile("(\\+?)(\\D*)([0-9]?)(\\D*)(9([0-9]{2}))(\\D*)([0-9]{3})(\\D*)([0-9]{2})(\\D*)([0-9]{2})");
+        while (true) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
+
+            if (input.equals("0")) {
+                break;
+            }
+
+            if (matcher.matches()) {
+                correctPhoneNumber = matcher.replaceFirst("7$5$8$10$12");
+            } else {
+                correctPhoneNumber = "Неверный формат номера";
+            }
+
+            System.out.println(correctPhoneNumber);
+        }
     }
-  }
 
 }
