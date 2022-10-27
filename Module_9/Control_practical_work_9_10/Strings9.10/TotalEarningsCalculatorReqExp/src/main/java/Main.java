dd37f30a3e0a300c8e20119cfa108b1c47662812
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-27 12:48:17 +0300

    Выполнил задание 3 (проект TotalEarningsCalculatorReqExp) в контрольной практической работе 9.10. Все тесты проходят.

--- Module_9/Control_practical_work_9_10/Strings9.10/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ Module_9/Control_practical_work_9_10/Strings9.10/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,28 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        System.out.println("Общая сумма: " + calculateSalarySum(text));
+    }
 
-  }
+    public static int calculateSalarySum(String text) {
+        int sum = 0;
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+        for (int i = 0; i < text.length(); i++) {
+            if (text.indexOf(' ') > 0) {
+                String currentSubstring = text.substring(0, text.indexOf(' '));
+                text = text.substring(currentSubstring.length() + 1);
+                if (isNumber(currentSubstring)) {
+                    sum += Integer.parseInt(currentSubstring);
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    public static boolean isNumber(String substring) {
+        return substring.matches("\\d+");
+    }
 
 }
