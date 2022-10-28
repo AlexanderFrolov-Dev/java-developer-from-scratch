0d835908658694f31f03e0e6cf4877fb4880b572
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-28 17:57:55 +0300

    Выполнил задание 3 (проект TwoDimensionalArray) в контрольной практической работе 10.5. Все тесты проходят.

--- Module_10/Control_practical_work_10_5/CollectionsArray/src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
+++ Module_10/Control_practical_work_10_5/CollectionsArray/src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
@@ -3,15 +3,22 @@ package practice.twoDimensionalArray;
 public class TwoDimensionalArray {
 
     public static final char SYMBOL = 'X';
+    public static final char SPACE = ' ';
 
     public static char[][] getTwoDimensionalArray(int size) {
+        char[][] cross = new char[size][size];
 
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ SYMBOL по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
+        for (int i = 0; i < cross.length; i++) {
+            for (int j = 0; j < cross[i].length; j++) {
+                if (i == j) {
+                    cross[i][j] = SYMBOL;
+                    cross[i][size - 1 - j] = SYMBOL;
+                } else if (cross[i][j] != SYMBOL) {
+                    cross[i][j] = SPACE;
+                }
+            }
+        }
 
-        return new char[0][0];
+        return cross;
     }
 }
