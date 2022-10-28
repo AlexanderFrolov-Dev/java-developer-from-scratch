d718593b0bd7af810b418a54aa5f5af3e8189940
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-10-28 14:24:22 +0300

    Выполнил задание 2 (проект Hospital) в контрольной практической работе 10.5. Все тесты проходят.

--- Module_10/Control_practical_work_10_5/CollectionsArray/src/main/java/practice/hospital/Hospital.java
+++ Module_10/Control_practical_work_10_5/CollectionsArray/src/main/java/practice/hospital/Hospital.java
@@ -1,24 +1,38 @@
 package practice.hospital;
 
 public class Hospital {
+    public static final float MIN_TEMPERATURE = 32.0f;
+    public static final float MAX_TEMPERATURE = 40.0f;
+    public static final float INTERVAL_TEMPERATURE = MAX_TEMPERATURE - MIN_TEMPERATURE;
+    public static final float MIN_NORMAL_TEMPERATURE = 36.2f;
+    public static final float MAX_NORMAL_TEMPERATURE = 36.9f;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        return new float[0];
+        float[] patientsTemperatures = new float[patientsCount];
+
+        for (int i = 0; i < patientsTemperatures.length; i++) {
+            patientsTemperatures[i] = Math.round((MIN_TEMPERATURE + INTERVAL_TEMPERATURE * Math.random()) * 10.0f) / 10.0f;
+        }
+
+        return patientsTemperatures;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-            Округлите среднюю температуру с помощью Math.round до 2 знаков после запятой,
-            а температуры каждого пациента до 1 знака после запятой
-        */
 
-        String report =
-            "Температуры пациентов: " + 1 +
-            "\nСредняя температура: " + 0 +
-            "\nКоличество здоровых: " + 0;
+        StringBuilder temperature = new StringBuilder();
+        float sum = 0.0f;
+        int numberOfHealthyPeople = 0;
 
-        return report;
+        for (float temperatureDatum : temperatureData) {
+            temperature.append(temperatureDatum).append(" ");
+            sum += temperatureDatum;
+            if (temperatureDatum >= MIN_NORMAL_TEMPERATURE && temperatureDatum <= MAX_NORMAL_TEMPERATURE) {
+                numberOfHealthyPeople++;
+            }
+        }
+
+        return "Температуры пациентов: " + temperature.toString().trim() +
+        "\nСредняя температура: " + Math.round((sum / temperatureData.length) * 100.0f) / 100.0f +
+        "\nКоличество здоровых: " + numberOfHealthyPeople;
     }
 }
