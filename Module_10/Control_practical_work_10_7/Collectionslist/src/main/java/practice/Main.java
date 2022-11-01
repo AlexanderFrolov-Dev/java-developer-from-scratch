486937fd83e2490107781ec0fa92e77c343a8692
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-01 17:51:24 +0300

    10.7.2 незаконченное

--- Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/Main.java
+++ Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/Main.java
@@ -1,9 +1,42 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
+    /*
+    ADD buy milk
+    ADD learn java
+    LIST
+     */
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
+        System.out.println("Введите команду:");
+
+        while (true) {
+            String input = new Scanner(System.in).nextLine();
+            String[] words = input.split(" ");
+
+            switch (words[0]) {
+                case ("ADD"):
+                    todoList.add(input.substring(input.indexOf(' ') + 1));
+                    System.out.println("Добавлено дело \"" + input.substring(input.indexOf(' ') + 1) + "\"");
+                    break;
+                case ("EDIT"):
+//                    System.out.println("EDIT"
+//                            + words[1]
+//                            + todoList.edit(Integer.parseInt(words[1]), input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')))
+//                            + input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')));
+                    break;
+                case ("LIST"):
+                    for (int i = 0; i < todoList.getTodos().size(); i++) {
+                        System.out.println(i + " - " + todoList.getTodos().get(i));
+                    }
+                    break;
+                case ("DELETE"):
+                    System.out.println("Дело " + "\"" + input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')) + "\"");
+            }
+        }
         // TODO: написать консольное приложение для работы со списком дел todoList
     }
 }
--- Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/TodoList.java
+++ Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/TodoList.java
@@ -3,29 +3,45 @@ package practice;
 import java.util.ArrayList;
 
 public class TodoList {
+    ArrayList<String> list = new ArrayList<>();
 
     public void add(String todo) {
+        list.add(todo);
         // TODO: добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
+        if (index < list.size()) {
+            list.add(index, todo);
+        } else {
+            add(todo);
+        }
+
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
     }
 
     public void edit(int index, String todo) {
+        if (index < list.size()) {
+            list.set(index, todo);
+        }
+
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
     }
 
     public void delete(int index) {
+        if (index < list.size()) {
+            list.remove(index);
+        }
+
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return list;
     }
 
 }
