9b154a18f03e6700f3df0e81b94aaa472a1487b7
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-02 16:46:01 +0300

    10.7 вывод в консоль корректный, но судя по заданию строки для вывода вероятно должны быть прописаны в соответствующих методах класса TodoList.

--- Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/Main.java
+++ Module_10/Control_practical_work_10_7/Collectionslist/src/main/java/practice/Main.java
@@ -3,41 +3,80 @@ package practice;
 import java.util.Scanner;
 
 public class Main {
-    /*
-    ADD buy milk
-    ADD learn java
-    LIST
-     */
-    private static TodoList todoList = new TodoList();
+    private static final TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         System.out.println("Введите команду:");
 
         while (true) {
             String input = new Scanner(System.in).nextLine();
-            String[] words = input.split(" ");
+            String[] commandElements = input.split(" ");
 
-            switch (words[0]) {
+            switch (commandElements[0]) {
                 case ("ADD"):
-                    todoList.add(input.substring(input.indexOf(' ') + 1));
-                    System.out.println("Добавлено дело \"" + input.substring(input.indexOf(' ') + 1) + "\"");
+                    if (isDigits(commandElements[1])) {
+                        todoList.add(Integer.parseInt(commandElements[1]), getCase(commandElements));
+                    } else {
+                        todoList.add(getCase(commandElements));
+                        System.out.println("Добавлено дело \""
+                                .concat(getCase(commandElements))
+                                .concat("\""));
+                    }
                     break;
                 case ("EDIT"):
-//                    System.out.println("EDIT"
-//                            + words[1]
-//                            + todoList.edit(Integer.parseInt(words[1]), input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')))
-//                            + input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')));
-
+                    String oldCase = todoList.getTodos().get(Integer.parseInt(commandElements[1]));
+                    todoList.edit(Integer.parseInt(commandElements[1]), getCase(commandElements));
+                    System.out.println("Дело \""
+                            .concat(oldCase)
+                            .concat("\"")
+                            .concat(" заменено на \"")
+                            .concat(getCase(commandElements))
+                            .concat("\""));
                     break;
                 case ("LIST"):
                     for (int i = 0; i < todoList.getTodos().size(); i++) {
-                        System.out.println(i + " - " + todoList.getTodos().get(i));
+                        System.out.print(i + " - " + todoList.getTodos().get(i) + System.lineSeparator());
                     }
                     break;
                 case ("DELETE"):
-                    System.out.println("Дело " + "\"" + input.substring(input.indexOf(' ') + 1).substring(input.indexOf(' ')) + "\"");
+                    if (Integer.parseInt(commandElements[1]) < todoList.getTodos().size()) {
+                        String oldValue = todoList.getTodos().get(Integer.parseInt(commandElements[1]));
+                        todoList.delete(Integer.parseInt(commandElements[1]));
+                        System.out.println("Дело \"".concat(oldValue).concat("\" удалено"));
+                    } else {
+                        System.out.println("Дело с таким номером не существует");
+                    }
+
             }
         }
         // TODO: написать консольное приложение для работы со списком дел todoList
+    }
+
+    private static boolean isDigits(String text) {
+        boolean number = true;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isDigit(text.charAt(i))) {
+                number = false;
+            }
+        }
+
+        return number;
+    }
+
+    private static String getCase(String[] array) {
+        StringBuilder sb = new StringBuilder();
+
+        if (isDigits(array[1])) {
+            for (int i = 2; i < array.length; i++) {
+                sb.append(array[i]).append(" ");
+            }
+        } else {
+            for (int i = 1; i < array.length; i++) {
+                sb.append(array[i]).append(" ");
+            }
+        }
+
+        return sb.toString().trim();
     }
 }
