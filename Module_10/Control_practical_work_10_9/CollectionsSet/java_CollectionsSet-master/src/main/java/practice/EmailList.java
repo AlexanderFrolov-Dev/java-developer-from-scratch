1fe0a5a5bb9356bf0876ac404e00dde1bf4b9937
Aleksandr Frolov <timonfaa@mail.ru>
2022-11-04 19:56:22 +0300

    Выполнил задание в практической работе 10.9. Все тесты проходят. Вывод в консоль также корректный.

--- Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/EmailList.java
+++ Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/EmailList.java
@@ -8,37 +8,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailList {
-
-    private Set<String> set = new TreeSet<>();
+    private final Set<String> setEmail = new TreeSet<>();
 
     public void add(String email) {
         Pattern pattern = Pattern.compile("\\w+@\\w+\\.\\w+");
         Matcher matcher = pattern.matcher(email.toLowerCase());
 
-        if (matcher.matches() && !set.contains(email.toLowerCase())) {
-            set.add(email.toLowerCase());
+        if (matcher.matches()) {
+            setEmail.add(email.toLowerCase());
+        } else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
         }
-        // TODO: валидный формат email добавляется, email это строка, она быть может любой
-        // принять решение добавлять аргумент email или нет должен этот метод
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-//        TreeSet<String> set = new TreeSet<>();
-//        for (String s : list) {
-//            set.add(s);
-//        }
-//
-//        list.clear();
-//
-//        for (int i = 0; i < set.size(); i++) {
-//            if (set.iterator().hasNext()) {
-//                list.add(set.iterator().next());
-//            }
-//        }
-
-
-        return new ArrayList<>(set);
+        return new ArrayList<>(setEmail);
     }
 
 }
--- Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/Main.java
+++ Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/Main.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
-        System.out.println("Введите email");
+        System.out.println("Введите email:");
         Scanner scanner = new Scanner(System.in);
-        
+        EmailList emailList = new EmailList();
+
         while (true) {
             String input = scanner.nextLine();
+            String[] commandElements = input.split(" ");
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+
+            if (commandElements[0].equals("ADD") && commandElements.length == 2) {
+                emailList.add(commandElements[1]);
+            }
+
+            if (commandElements[0].equals("LIST") && commandElements.length == 1) {
+                for (String email : emailList.getSortedEmails()) {
+                    System.out.println(email);
+                }
+            }
         }
     }
+
 }
