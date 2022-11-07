d945d2411cbd6ee7f90c4ba3852e83246f80e4d5
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-07 18:04:06 +0300

    Начал выполнять 10.11

--- Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
+++ Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
@@ -1,13 +1,26 @@
 package practice;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
+    Map<String, String> phoneBook = new TreeMap<>();
 
     public void addContact(String phone, String name) {
+        if (phoneBook.containsValue(phone)) {
+            phoneBook.put((phoneBook.get(phone).replaceAll(phoneBook.get(phone), name)), phone);
+        }
+
+        if (isPhone(phone) && isName(name)) {
+            phoneBook.put(name, phone);
+        }
+
         // проверьте корректность формата имени и телефона
-        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
+        // (рекомендуется написать отдельные методы для проверки является строка именем/телефоном)
         // если такой номер уже есть в списке, то перезаписать имя абонента
     }
 
@@ -24,9 +37,13 @@ public class PhoneBook {
     }
 
     public Set<String> getAllContacts() {
+        Set<String> listContacts = new TreeSet<>();
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            listContacts.add(entry.getKey() + " - " + entry.getValue());
+        }
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        return listContacts;
     }
 
     // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
@@ -37,4 +54,17 @@ public class PhoneBook {
             String value = entry.getValue(); // получения ключа
         }
     */
+    private boolean isName(String name) {
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+        Matcher matcher = pattern.matcher(name.trim());
+
+        return matcher.matches();
+    }
+
+    private boolean isPhone(String phone) {
+        Pattern pattern = Pattern.compile("79\\d{9}");
+        Matcher matcher = pattern.matcher(phone);
+
+        return matcher.matches();
+    }
 }
