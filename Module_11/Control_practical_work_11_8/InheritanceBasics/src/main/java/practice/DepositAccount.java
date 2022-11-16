31e1e55acec92f701574077d13e84373a9eaf90c
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-16 13:56:12 +0300

    11.8.1 реализовал методы класса DepositAccount. Все тесты этого класса выполняются успешно.

--- Module_11/Control_practical_work_11_8/InheritanceBasics/src/main/java/practice/DepositAccount.java
+++ Module_11/Control_practical_work_11_8/InheritanceBasics/src/main/java/practice/DepositAccount.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class DepositAccount extends BankAccount {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class DepositAccount extends BankAccount {
+    LocalDate lastIncome;
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(lastIncome, currentDate);
+        if (period.getMonths() >= 1 || period.getYears() >= 1) {
+            super.take(amountToTake);
+        }
+    }
 }
