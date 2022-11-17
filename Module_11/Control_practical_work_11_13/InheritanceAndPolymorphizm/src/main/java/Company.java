032629acb8b31c7494d0cdae199d56ee36f77b69
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-17 16:50:51 +0300

    Добавил папку Control_practical_work_11_13 и проект в ней InheritanceAndPolymorphizm.

---
+++ Module_11/Control_practical_work_11_13/InheritanceAndPolymorphizm/src/main/java/Company.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Company implements Employee{
+    public ArrayList<Employee> getTopSalaryStaff(int count) {
+        return null;
+    }
+
+    public ArrayList<Employee> getLowerSalaryStaff(int count) {
+        return null;
+    }
+
+    @Override
+    public int getMonthSalary() {
+        return 0;
+    }
+}
