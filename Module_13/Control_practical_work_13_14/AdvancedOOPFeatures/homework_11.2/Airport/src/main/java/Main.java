352764a189a0be695659586989239a6b5507a89f
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-11-28 16:50:44 +0300

    13.14 выполнил задание. Все тесты завершаются успешно.

--- Module_13/Control_practical_work_13_14/AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ Module_13/Control_practical_work_13_14/AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,17 +1,36 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        Date currentDate = new Date();
+        long TWO_HOURS = 7_200_000;
+        System.out.println(currentDate.getTime());
+        long twoHoursEarlier = currentDate.getTime() - TWO_HOURS;
+        System.out.println(twoHoursEarlier);
 
-
+        for (Flight flight : findPlanesLeavingInTheNextTwoHours(Airport.getInstance())) {
+            System.out.println(flight);
+        }
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
+        long TWO_HOURS = 7_200_000;
+
+        return airport.getTerminals().stream()
+                .map(Terminal::getFlights)
+                .flatMap(Collection::stream)
+                .filter(flight -> flight.getDate().getTime() <= (new Date().getTime() + TWO_HOURS))
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .collect(Collectors.toList());
     }
 
 }
