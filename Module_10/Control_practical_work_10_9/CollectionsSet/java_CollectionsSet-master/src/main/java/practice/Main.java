e44506357f53327e7aa4b50f0ea898f0091978b2
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-12-14 16:53:49 +0300

    10.9 создал переменную для записи в нее значения массива commandElements[0].

--- Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/Main.java
+++ Module_10/Control_practical_work_10_9/CollectionsSet/java_CollectionsSet-master/src/main/java/practice/Main.java
@@ -14,15 +14,16 @@ public class Main {
         while (true) {
             String input = scanner.nextLine();
             String[] commandElements = input.split(" ");
+            String command = commandElements[0];
             if (input.equals("0")) {
                 break;
             }
 
-            if (commandElements[0].equals("ADD") && commandElements.length == 2) {
+            if (command.equals("ADD") && commandElements.length == 2) {
                 emailList.add(commandElements[1]);
             }
 
-            if (commandElements[0].equals("LIST") && commandElements.length == 1) {
+            if (command.equals("LIST") && commandElements.length == 1) {
                 List<String> emails = emailList.getSortedEmails();
                 for (String email : emails) {
                     System.out.println(email);
