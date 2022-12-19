786a31374f63f61b01dd9ead39133d9b4636a66e
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-12-19 17:15:50 +0300

    15.4 Создал папку для 15 модуля и в ней папку Practical_work_15_4 с проектом домашнего задания FolderSizeMeter.

---
+++ Module_15/Practical_work_15_4/FolderSizeMeter/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("Введите путь к папке или файлу");
+            String path = new Scanner(System.in).nextLine();
+        }
+    }
+
+    public static double getFolderSize() {
+        return 0;
+    }
+}
