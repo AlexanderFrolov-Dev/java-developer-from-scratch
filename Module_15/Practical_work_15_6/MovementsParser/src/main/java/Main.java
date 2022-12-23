c47ed3d1d71f19a947a43ed46390935531ae9bb8
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-12-23 16:00:39 +0300

    15.6 Реализовал методы валидации количества элементов в строках файла и типа данных значений столбцов "Приход" и "Расход". Вывод результатов работы программы корректный, за исключением вывода описания операции CARD2CARD ALFA_MOBILE>MOSCOW. Должно быть просто MOSCOW (только сейчас обнаружил это в задании).

--- Module_15/Practical_work_15_6/MovementsParser/src/main/java/Main.java
+++ Module_15/Practical_work_15_6/MovementsParser/src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static final int INDEX_OPERATION_DESCRIPTION_COLUMN = 5;
     public static final int INDEX_RECEIPT_OF_MONEY_COLUMN = 6;
     public static final int INDEX_SPENDING_MONEY_COLUMN = 7;
+    public static final int VALID_COLUMNS_COUNT = 8;
     static Pattern operationDescription = Pattern.compile("([\\\\/])(((\\w+>?\\w*)+(\\s+))+)((\\d{2}\\.){2}\\d{2})");
 
     public static void main(String[] args) {
@@ -23,25 +24,34 @@ public class Main {
             ex.printStackTrace();
         }
 
-        columnsCount = getTitles(rows.get(0)).size();
-        rowsWithoutTitles = rows.subList(1, rows.size());
-        uniqueOperationDescriptions = getUniqueOperationDescription(rows);
+        if (isValidColumnsCount(rows)) {
+            columnsCount = getTitles(rows).size();
+            rowsWithoutTitles = rows.subList(1, rows.size());
+            uniqueOperationDescriptions = getUniqueOperationDescription(rows);
 
-        for (Map.Entry<String, Double> entry : getSumOfTransactionsByTypes(rowsWithoutTitles, INDEX_RECEIPT_OF_MONEY_COLUMN).entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
+            System.out.println("Сумма по операциям " + getTitles(rows).get(INDEX_RECEIPT_OF_MONEY_COLUMN) + "а по типам:");
+            for (Map.Entry<String, Double> entry : getSumOfTransactionsByTypes(rowsWithoutTitles, INDEX_RECEIPT_OF_MONEY_COLUMN).entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+            System.out.println("--------------");
+            System.out.println("Общая сумма по операциям " + getTitles(rows).get(INDEX_RECEIPT_OF_MONEY_COLUMN) + "а: "
+                    + getTotalSumOfTransactionsByTypes(rowsWithoutTitles, INDEX_RECEIPT_OF_MONEY_COLUMN));
+
+            System.out.println("===============");
+
+            System.out.println("Сумма по операциям " + getTitles(rows).get(INDEX_SPENDING_MONEY_COLUMN) + "а по типам:");
+            for (Map.Entry<String, Double> entry : getSumOfTransactionsByTypes(rowsWithoutTitles, INDEX_SPENDING_MONEY_COLUMN).entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+            System.out.println("--------------");
+            System.out.println("Общая сумма по операциям " + getTitles(rows).get(INDEX_SPENDING_MONEY_COLUMN) + "а: "
+                    + getTotalSumOfTransactionsByTypes(rowsWithoutTitles, INDEX_SPENDING_MONEY_COLUMN));
         }
-
-//        System.out.println(rows.size());
-//        System.out.println(rowsWithoutTitles.size());
-//        System.out.println(getOperationDescription(rows.get(4)));
-//        for (String s : uniqueOperationDescriptions) {
-//            System.out.println(s);
-//        }
 
     }
 
-    private static List<String> getTitles(String firstRow) {
-        return Arrays.asList(firstRow.split(","));
+    private static List<String> getTitles(List<String> rows) {
+        return Arrays.asList(rows.get(0).split(","));
     }
 
     private static String getOperationDescription(String row) {
@@ -72,31 +82,51 @@ public class Main {
             Matcher matcher = operationDescription.matcher(valuesOfCurrentRow.get(INDEX_OPERATION_DESCRIPTION_COLUMN));
             String currentTypeOfOperation = getOperationDescription(valuesOfCurrentRow.get(INDEX_OPERATION_DESCRIPTION_COLUMN));
 
-            if (matcher.find()) {
+            if (matcher.find() && isDoubleType(valuesOfCurrentRow.get(indexOfOperationColumn))) {
                 sumOfTransactionsByTypes
                         .put(currentTypeOfOperation, sumOfTransactionsByTypes.containsKey(currentTypeOfOperation)
                                 ? sumOfTransactionsByTypes.get(currentTypeOfOperation) + Double.parseDouble(valuesOfCurrentRow.get(indexOfOperationColumn))
                                 : Double.parseDouble(valuesOfCurrentRow.get(indexOfOperationColumn)));
-
             }
-
-//            if (matcher.find()) {
-//                sumOfTransactionsByTypes
-//                        .put(currentTypeOfOperation, () -> {
-//                            if (sumOfTransactionsByTypes.containsKey(currentTypeOfOperation)) {
-//                                return sumOfTransactionsByTypes.get(currentTypeOfOperation) + Double.parseDouble(valuesOfCurrentRow.get(indexOfOperationColumn));
-//                            } else {
-//                                return Double.parseDouble(valuesOfCurrentRow.get(indexOfOperationColumn));
-//                            }
-//
-//                        });
-//
-//            }
-//.get(currentTypeOfOperation)
 
         }
 
         return sumOfTransactionsByTypes;
+    }
+
+    private static double getTotalSumOfTransactionsByTypes(List<String> rows, int indexOfOperationColumn) {
+        double totalSum = 0;
+
+        for (String s : rows) {
+            String[] currentRow = s.split(",");
+            if (isDoubleType(currentRow[indexOfOperationColumn])) {
+                totalSum += Double.parseDouble(currentRow[indexOfOperationColumn]);
+            }
+        }
+
+        return totalSum;
+    }
+
+    private static boolean isValidColumnsCount(List<String> rows) {
+        boolean validRow = true;
+
+        for (String s : rows) {
+            if (s.split(",").length != Main.VALID_COLUMNS_COUNT) {
+                validRow = false;
+                break;
+            }
+        }
+
+        return validRow;
+    }
+
+    private static boolean isDoubleType(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 
 }
