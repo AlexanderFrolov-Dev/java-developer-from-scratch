caba52cb2e1183452aee01362533fc7570406f1f
Aleksandr Frolov <frolov-departament-dev@mail.ru>
2022-12-26 17:10:38 +0300

    10.11 переписал класс PhoneBook так, чтобы ключём в мапе было имя, а значением номер телефона. Метод Main пока закомментировал, им займусь позже.

--- Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
+++ Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
@@ -1,78 +1,112 @@
 package practice;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBook {
-    Map<String, String> phoneBook = new TreeMap<>();
-    Map<String, Set<String>> namePhoneOrder = new TreeMap<>();
+    Map<String, Set<String>> phoneBookList = new TreeMap<>();
 
     public void addContact(String phone, String name) {
-        if (isPhone(phone) && isName(name)) {
-            phoneBook.put(phone, name);
-        } else if (phoneBook.containsKey(phone)) {
-            phoneBook.put(phone, name);
+        // проверьте корректность формата имени и телефона
+        // (рекомендуется написать отдельные методы для проверки является строка именем/телефоном)
+        // если такой номер уже есть в списке, то перезаписать имя абонента
+        boolean containPhone = false;
+        boolean needRemove = false;
+        for (Map.Entry<String, Set<String>> entry : phoneBookList.entrySet()) {
+            for (String s : entry.getValue()) {
+                if (s.equals(phone)) {
+                    containPhone = true;
+                }
+            }
+        }
+
+        if (isValidName(name) && isValidPhone(phone) && !phoneBookList.containsKey(name) && !containPhone) {
+            phoneBookList.put(name, new TreeSet<>());
+            phoneBookList.get(name).add(phone);
+        } else if (isValidName(name) && isValidPhone(phone) && !phoneBookList.containsKey(name) && containPhone) {
+            for (Map.Entry<String, Set<String>> entry : phoneBookList.entrySet()) {
+                Set<String> currentSetPhones = entry.getValue();
+                for (String s : currentSetPhones) {
+                    if (s.equals(phone) && currentSetPhones.size() > 1) {
+                        phoneBookList.get(entry.getKey()).remove(phone);
+                    } else if (s.equals(phone) && currentSetPhones.size() == 1) {
+                        needRemove = true;
+                    }
+                }
+                if (needRemove) {
+                    phoneBookList.remove(entry.getKey());
+                }
+            }
+            Set<String> phones = new TreeSet<>();
+            phones.add(phone);
+            phoneBookList.put(name, phones);
+        }
+        else if (isValidName(name) && isValidPhone(phone) && phoneBookList.containsKey(name)) {
+            phoneBookList.get(name).add(phone);
         }
     }
 
     public String getContactByPhone(String phone) {
-        return phoneBook.get(phone) + " - " + phone;
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найдены - вернуть пустую строку
+        String contactByPhone = "";
+
+        for (Map.Entry<String, Set<String>> entry : phoneBookList.entrySet()) {
+            for (String s : entry.getValue()) {
+                if (s.equals(phone)) {
+                    contactByPhone = entry.getKey() + " - " + String.join(",", entry.getValue());
+                }
+            }
+        }
+
+        return contactByPhone;
     }
 
     public Set<String> getContactsByName(String name) {
-        Set<String> setContacts = new TreeSet<>();
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            if (entry.getValue().equals(name)) {
-                setContacts.add(name + " - " + entry.getKey());
-            }
-        }
-        return setContacts;
+        // формат одного контакта "Имя - Телефон"
+        // если контакт не найден - вернуть пустой TreeSet
+        Set<String> contactByName = new TreeSet<>();
+        contactByName.add(name + " - " + String.join(",", phoneBookList.get(name)));
+        return contactByName;
     }
 
     public Set<String> getAllContacts() {
-        Set<String> contacts = new TreeSet<>();
-
-        addAllNumbersByContact();
-
-        for (Map.Entry<String, Set<String>> entry : namePhoneOrder.entrySet()) {
-            String[] array = new String[entry.getValue().size()];
-            String[] subscriberNumbers = entry.getValue().toArray(array);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < subscriberNumbers.length; i++) {
-                if (i < entry.getValue().size() - 1) {
-                    sb.append(subscriberNumbers[i]).append(", ");
-                } else {
-                    sb.append(subscriberNumbers[i]);
-                }
-            }
-            contacts.add(entry.getKey() + " - " + sb);
+        // формат одного контакта "Имя - Телефон"
+        // если контактов нет в телефонной книге - вернуть пустой TreeSet
+        Set<String> contactsList = new TreeSet<>();
+        String result;
+        for (Map.Entry<String, Set<String>> entry : phoneBookList.entrySet()) {
+            result = entry.getKey() + " - ";
+            Set<String> values = entry.getValue(); // получения ключа
+            result += String.join(", ", values);
+            contactsList.add(result);
         }
-
-        return contacts;
+        return contactsList;
     }
 
-    public boolean isName(String name) {
-        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
-        Matcher matcher = pattern.matcher(name.trim());
+    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
+    // это поможет вам найти все ключи (key) по значению (value)
+    /*
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            String key = entry.getKey(); // получения ключа
+            String value = entry.getValue(); // получения ключа
+        }
+    */
 
+    private boolean isValidName(String name) {
+        Pattern validName = Pattern.compile("[А-Я][а-я]+");
+        Matcher matcher = validName.matcher(name);
         return matcher.matches();
     }
 
-    public boolean isPhone(String phone) {
-        Pattern pattern = Pattern.compile("79\\d{9}");
-        Matcher matcher = pattern.matcher(phone);
-
+    private boolean isValidPhone(String phone) {
+        Pattern validNumber = Pattern.compile("79\\d{9}");
+        Matcher matcher = validNumber.matcher(phone);
         return matcher.matches();
     }
 
-    private void addAllNumbersByContact() {
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            namePhoneOrder.put(entry.getValue(), new TreeSet<>());
-        }
-
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            namePhoneOrder.get(entry.getValue()).add(entry.getKey());
-        }
-    }
 }
