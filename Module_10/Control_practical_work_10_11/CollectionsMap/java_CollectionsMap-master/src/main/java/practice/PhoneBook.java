0e49740b054de6480d11cf4c204b1e6d1ad5091c
AlexanderFrolov-spec <84732086+AlexanderFrolov-spec@users.noreply.github.com>
2023-01-02 12:35:52 +0300

    10.11 удалил закомменченный код, и поправил список импортов.

--- Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
+++ Module_10/Control_practical_work_10_11/CollectionsMap/java_CollectionsMap-master/src/main/java/practice/PhoneBook.java
@@ -1,6 +1,12 @@
 package practice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
